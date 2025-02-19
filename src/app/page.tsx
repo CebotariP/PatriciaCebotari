2e2f05106d2212be54e9130fafe7315dddb6a937
CebotariP <tricia0272@gmail.com>
2025-02-18 18:28:09 -0800

    setting up background image and changing buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,117 +2,62 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div
+      className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
+      style={{ backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('/back.JPG')", backgroundSize: "cover", backgroundPosition: "center" }}
+    >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {/* <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        /> */}
-        <div id="logo" className="w-[700px] text-[60px] justify-center flex gap-2 ">
-            <a href="" >
-                <span>Patricia</span><span> Cebotari</span>
-            </a>
-        </div>
+      <div id="logo" className="w-[700px] text-[60px] justify-center flex gap-2 ">
+      <a href="" >
+        <span>Patricia</span><span> Cebotari</span>
+      </a>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col place-self-center scale-125 sm:flex-row sm:gap-8">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              // className="dark:invert"
-              src="/Sprout Logo.png"
-              alt="Project Sprout Plant logomark"
-              width={20}
-              height={20}
-            />
-            Projects
-          </a>
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              // className="dark:invert"
-              src="/SproutGrowing.png"
-              alt="About Me: Sprout Plant Growing logomark"
-              width={20}
-              height={20}
-            />
-            About Me
-          </a>
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              // className="dark:invert"
-              src="/SproutSprouting.png"
-              alt="Contact Sprout Plant Sprouting logomark"
-              width={20}
-              height={20}
-            />
-            Contact
-          </a>
-        </div>
+      <div className="flex gap-4 items-center flex-col place-self-center scale-125 sm:flex-row sm:gap-8">
+      <a
+      className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-[rgba(0,0,0,0.5)] text-white gap-2 hover:bg-[rgba(0,0,0,0.7)] dark:hover:bg-[rgba(136, 71, 71, 0.5)] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+      href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+      target="_blank"
+      rel="noopener noreferrer"
+      >
+      <Image
+        src="/Sprout Logo.png"
+        alt="Project Sprout Plant logomark"
+        width={20}
+        height={20}
+      />
+      Projects
+      </a>
+      <a
+      className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-[rgba(0,0,0,0.5)] text-white gap-2 hover:bg-[rgba(0,0,0,0.7)] dark:hover:bg-[rgba(136, 71, 71, 0.5)] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+      href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+      target="_blank"
+      rel="noopener noreferrer"
+      >
+      <Image
+        src="/SproutGrowing.png"
+        alt="About Me: Sprout Plant Growing logomark"
+        width={20}
+        height={20}
+      />
+      About Me
+      </a>
+      <a
+      className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-[rgba(0,0,0,0.5)] text-white gap-2 hover:bg-[rgba(0,0,0,0.7)] dark:hover:bg-[rgba(136, 71, 71, 0.5)] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+      href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+      target="_blank"
+      rel="noopener noreferrer"
+      >
+      <Image
+        src="/SproutSprouting.png"
+        alt="Contact Sprout Plant Sprouting logomark"
+        width={20}
+        height={20}
+      />
+      Contact
+      </a>
+      </div>
       </main>
-      {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer> */}
     </div>
   );
 }
